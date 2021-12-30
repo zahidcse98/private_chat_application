3c1981250a9517b4ff0258d109bf5803cadd1551
Zahid Hasan <zahidcse98@gmail.com>
2021-12-30 19:22:36 +0600

    lesson 2 completed successfully

---
+++ middlewares/users/userValidators.js
@@ -0,0 +1,79 @@
+//external imports
+const { check, validationResult } = require("express-validator");
+const createError = require("http-errors");
+const path = require("path");
+const { unlink } = require("fs");
+
+//internal imports
+const User = require("../../models/People");
+
+//add user
+const addUserValidators = [
+  check("name")
+    .isLength({ min: 1 })
+    .withMessage("Name is Required")
+    .isAlpha("en-US", { ignore: " -" })
+    .withMessage("Name must not contain anything than alphabet")
+    .trim(),
+  check("email")
+    .isEmail()
+    .withMessage("Invalid email address")
+    .trim()
+    .custom(async (value) => {
+      try {
+        const user = await User.findOne({ email: value });
+        if (user) {
+          throw createError("Email already in use");
+        }
+      } catch (err) {
+        throw createError(err.message);
+      }
+    }),
+  check("mobile")
+    .isMobilePhone("bn-BD", {
+      strictMode: true,
+    })
+    .withMessage("Mobile number must be a valid Bangladeshi mobile number")
+    .custom(async (value) => {
+      try {
+        const user = await User.findOne({ mobile: value });
+        if (user) {
+          throw createError("Mobile is already in use!!");
+        }
+      } catch (err) {
+        throw createError(err.message);
+      }
+    }),
+  check("password")
+    .isStrongPassword()
+    .withMessage(
+      "Password must be contain at least 8 characters ling & should contain 1 lowercase, 1 uppercase, 1 number & 1 symbol"
+    ),
+];
+
+const addUserValidationHandler = function (req, res, next) {
+  const errors = validationResult(req);
+  const mappedErrors = errors.mapped();
+  if (Object.keys(mappedErrors).length === 0) {
+    next();
+  } else {
+    //remove upload files
+    if (req.files.length > 0) {
+      const { filename } = req.files[0];
+      unlink(
+        path.join(__dirname, `/../public/uploads/avatars/${filename}`),
+        (err) => {
+          if (err) console.log(err);
+        }
+      );
+    }
+
+    res.status(500).json({
+      errors: mappedErrors,
+    });
+  }
+};
+module.exports = {
+  addUserValidators,
+  addUserValidationHandler,
+};
